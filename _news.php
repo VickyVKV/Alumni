1fbfaee864d63d731f53611a6f510e887ec4d004
sanzmata1834 <sanzmata1834@he-ferrer.eu>
2018-11-17 21:10:40 +0100

    Ajout couleurs catégories et mise en page de section actualité

--- _news.php
+++ _news.php
@@ -6,37 +6,42 @@
 ?>
 
 <div class="row filter">
-						<div id="accueil" class="button col-12 col-md-1 mt-md-0 mt-2 tri" data-filter="article"><a>Tout</a></div>
-						<div id="art" class="button col-12 col-md-2 mt-md-0 mt-2 tri" data-filter="arts-appliques"><a>Arts appliqués</a></div>
-						<div id="eco" class="button col-12 col-md-2 mt-md-0 mt-2 tri" data-filter="economique-cooremans"><a>Economie</a></div>
-						<div id="para" class="button col-12 col-md-2 mt-md-0 mt-2 tri" data-filter="paramedical"><a>Paramédicale</a></div>
-						<div  id="peda" class="button col-12 col-md-2 mt-md-0 mt-2 tri" data-filter="pedagogique-bulls"><a>Pédagogique</a></div>
-						<div id="social" class="button col-12 col-md-1 mt-md-0 mt-2 tri" data-filter="social-cooremans"><a>Social</a></div>
-						<div id="tech" class="button col-12 col-md-2 mt-md-0 mt-2 tri" data-filter="technique"><a>Technique</a></div>
+						<div id="accueil" class="button col-12 col-md-1 mt-md-0 mt-2 text-center" data-filter="article"><a>Tout</a></div>
+						<div id="art" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="arts-appliques"><a>Arts appliqués</a></div>
+						<div id="eco" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="economique-cooremans"><a>Economie</a></div>
+						<div id="para" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="paramedical"><a>Paramédicale</a></div>
+						<div  id="peda" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="pedagogique-bulls"><a>Pédagogique</a></div>
+						<div id="social" class="button col-12 col-md-1 mt-md-0 mt-2 text-center" data-filter="social-cooremans"><a>Social</a></div>
+						<div id="tech" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="technique"><a>Technique</a></div>
 					</div>
 		<div class="row news">
-			<div class="col-12"><h2>Actualités</h2>
+			<div class="col-12 my-5"><h2>Actualités</h2>
 			</div>
 			
 			<?php query_posts('category_name=actualite');
 					 while ( have_posts() ) : the_post(); ?>
-				<div class="article col-lg-4 col-md-6 col-sm-12 col- <?php foreach((get_the_category()) as $category) { echo $category->slug . ' '; } ?>">
+				<div class="actu article col-md-6 <?php foreach((get_the_category()) as $category) { echo $category->slug . ' '; } ?>">
 				<!-- article -->
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					
+					<div class="hashtags <?php foreach((get_the_category()) as $category) { echo $category->slug . ' '; } ?>">
+					
+					<?php foreach((get_the_category()) as $category) {
+					
+					if($category->slug  != "actualite" && $category->slug  != "evenement" && $category->slug  != "slider") {
+					
+					echo '<a  class="button2 '.$category->slug.'"  data-filter="'.$category->slug.'">' . $category->slug . '</a> '; 
+					
+					}
+					
+					} ?>
+					</div><!-- hashtags -->
 
-					<!-- post thumbnail -->
-					<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-							<?php the_post_thumbnail("medium"); // Declare pixel size you need inside the array ?>
-						</a>
-					<?php endif; ?>
-					<!-- /post thumbnail -->
-
-					<div class="news-content">
-				
+					<div class="p-3 news-content">	
+						
 					<!-- post title -->
 					<h3>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+						<a class="titre-actu" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 					</h3>
 					<!-- /post title -->
 
@@ -47,20 +52,6 @@
 					<!-- /post details -->
 
 					<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-					
-					
-						<div class="hashtags">
-					
-					<?php foreach((get_the_category()) as $category) {
-					
-					if($category->slug  != "actualite" && $category->slug  != "evenement" && $category->slug  != "slider") {
-					
-					echo '<a  class="button2 '.$category->slug.'"  data-filter="'.$category->slug.'">#' . $category->slug . '</a> '; 
-					
-					}
-					
-					} ?>
-					</div><!-- hashtags -->
 
 					<?php //edit_post_link(); ?>
 					</div><!--  news-content -->
@@ -72,4 +63,4 @@
 	wp_reset_query();?>
 
 			<?php //get_template_part('pagination'); ?>
-			</div> <!-- Row news -->
+			</div><!-- Row news -->
