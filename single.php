dcacd336d158c0e6ba7ca89fc85a8eb16fd3d3ae
sanzmata1834 <sanzmata1834@he-ferrer.eu>
2018-11-18 19:30:44 +0100

    Ajout de la page single pour actualité

--- single.php
+++ single.php
@@ -4,43 +4,86 @@
 	<!-- section -->
 	<section>
 
+	<div class="mb-5 container-fluid img-top-actu-detail"></div>	
+		
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
-
+		<article class="container" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+			<div class="row">
+				
 			<!-- post title -->
-			<h1>
+			<div class="col-7">
+			<h1 class="mb-3">
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 			</h1>
 			<!-- /post title -->
-
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
-
+			
+			<div>
 			<?php the_content(); // Dynamic Content ?>
+			</div>
+			</div>
+				
+			<div class="col-sm-5">
+			<div class="info-prat-actu">
+			
+			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span><br>
+			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+			<p><?php _e( 'Categorised in: <br>', 'html5blank' ); the_category(', '); // Separated by commas ?></p>	
+			</div>
+				
+			<div>
+			<h2 class="my-5">Plus d'actualités</h2>
+			<?php query_posts('category_name=actualite');
+					 while ( have_posts() ) : the_post(); ?>
+				<div class="actu articledetactu col-md-6 <?php foreach((get_the_category()) as $category) { echo $category->slug . ' '; } ?>">
+				<!-- article -->
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					
+					<div class="hashtags <?php foreach((get_the_category()) as $category) { echo $category->slug . ' '; } ?>">
+					
+					<?php foreach((get_the_category()) as $category) {
+					
+					if($category->slug  != "actualite" && $category->slug  != "evenement" && $category->slug  != "slider") {
+					
+					echo '<a  class="button2 '.$category->slug.'"  data-filter="'.$category->slug.'">' . $category->slug . '</a> '; 
+					
+					}
+					
+					} ?>
+					</div><!-- hashtags -->
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+					<div class="p-3 news-content">	
+						
+					<!-- post title -->
+					<h3>
+						<a class="titre-actu" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+					</h3>
+					<!-- /post title -->
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+					<!-- post details -->
+					<span class="date">Publié le <?php the_date(); ?> <?php the_time('g:i a'); ?></span>
+					<!--<span class="author"><?php _e( 'Published by', 'html5blank' ); ?><?php the_author_posts_link(); ?></span>-->
+					<!--<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>-->
+					<!-- /post details -->
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+					<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
-			<?php comments_template(); ?>
+					<?php //edit_post_link(); ?>
+					</div><!--  news-content -->
+				</article>
+				</div>
+				<!-- /article -->
 
+			  <?php endwhile;
+	wp_reset_query();?>
+			</div>
+			</div>
+			
+			</div>	
+				
 		</article>
 		<!-- /article -->
 
@@ -61,7 +104,5 @@
 	</section>
 	<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
