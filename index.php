e1d0146e448d85be5171cb39150b0578232e9258
anaelle <todde.anaelle@outlook.com>
2018-11-20 11:23:49 +0100

    changement anaelle2

--- index.php
+++ index.php
@@ -7,21 +7,20 @@
 		<?php include("_slider.php"); ?>
   		</section>
 		<!-- / SLIDER -->
-		<section class="container ct-news">
-	<br>
-		<section class="container">
+		
 		<div class="row filter">
-			<div id="accueil" class="button col-lg-1 col-md-12 col-sm-12 text-center" data-filter="article"><a>Tout</a></div>
-			<div id="art" class="button  col-lg-2 col-md-6 col-sm-12 text-center" data-filter="arts-appliques"><a>Arts appliqués</a></div>
-			<div id="eco" class="button  col-lg-2 col-md-6 col-sm-12 text-center" data-filter="economique-cooremans"><a>Economie</a></div>
-			<div id="para" class="button  col-lg-2 col-md-6 col-sm-12 text-center" data-filter="paramedical"><a>Paramédicale</a></div>
-			<div  id="peda" class="button  col-lg-2 col-md-6 col-sm-12 text-center" data-filter="pedagogique-bulls"><a>Pédagogique</a></div>
-			<div id="social" class="button  col-lg-1 col-md-6 col-sm-12 text-center" data-filter="social-cooremans"><a>Social</a></div>
-			<div id="tech" class="button col-lg-2 col-md-6 col-sm-12 text-center" data-filter="technique"><a>Technique</a></div>
+			<div id="accueil" class="button col-12 col-md-1 mt-md-0 mt-2 text-center" data-filter="article"><a>Tout</a></div>
+			<div id="art" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="arts-appliques"><a>Arts appliqués</a></div>
+			<div id="eco" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="economique-cooremans"><a>Economie</a></div>
+			<div id="para" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="paramedical"><a>Paramédicale</a></div>
+			<div  id="peda" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="pedagogique-bulls"><a>Pédagogique</a></div>
+			<div id="social" class="button col-12 col-md-1 mt-md-0 mt-2 text-center" data-filter="social-cooremans"><a>Social</a></div>
+			<div id="tech" class="button col-12 col-md-2 mt-md-0 mt-2 text-center" data-filter="technique"><a>Technique</a></div>
 		</div>
 		
 		<!-- section news -->
-
+		<section class="container ct-news">
+		
 		<div class="row news">
 			<div class="col-12 my-5"><h2>Actualités</h2>
 			</div>
@@ -79,8 +78,42 @@
 		
 		<!-- section events -->
 		<section class="container ct-events">
-			<?php include("_events.php"); ?>
-		</section>
+                
+                <div class="row">
+			<div class="col-12 my-5"><h2>Evenements</h2>
+			</div></div>        
+                
+			<div class="row justify-content-center">
+				<?php query_posts('category_name=evenement');
+					 while ( have_posts() ) : the_post(); ?>
+					<div class="article text-center col-10 col-lg-5 <?php foreach((get_the_category()) as $category) { echo $category->slug . ' '; } ?>">
+					<!-- article -->
+                        
+                        
+                        <div class="color">
+                        </div>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+						<!-- post thumbnail -->
+						<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+							</a>
+						<?php endif; ?>
+						<!-- /post thumbnail -->
+						
+				    
+					</article>
+                 
+					</div>
+					<!-- /article -->
+
+			  <?php endwhile;
+				wp_reset_query();?>
+
+			</div> <!-- Row events -->
+            </section>
+            
 		<!-- /section events -->
 		
 		<!-- section Galerie -->
@@ -89,5 +122,9 @@
 		</section>
 		<!-- /section galerie -->
 	</main>
+
+
+<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
